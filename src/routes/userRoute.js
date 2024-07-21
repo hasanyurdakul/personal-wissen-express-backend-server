105b59df97087ce130403f22f27f81c92f8ce36b
Hasan Yurdakul <hhasanyurdakul@gmail.com>
2024-07-21 22:35:02 +0300

    Update userRoute.js
    changed status to success

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -13,16 +13,16 @@ userRouter.post("/register", async (req, res) => {
     if (!username || !email || !password) {
       return res
         .status(404)
-        .send({ status: false, message: "Cannot leave empty fields!" });
+        .send({ success: false, message: "Cannot leave empty fields!" });
     }
     let user = await User.create(req.body);
     res.status(200).send({
-      status: true,
+      success: true,
       message: "User successfully created.",
       user: user,
     });
   } catch (error) {
-    res.status(400).send({ status: false, message: error.message });
+    res.status(400).send({ success: false, message: error.message });
   }
 });
 
@@ -32,7 +32,7 @@ userRouter.post("/login", async (req, res) => {
     if (!username || !password || username == "" || password == "") {
       return res
         .status(404)
-        .send({ staus: false, message: "Cannot leave empty fields!" });
+        .send({ success: false, message: "Cannot leave empty fields!" });
     }
     let userFromDb = await User.findOne({ username });
     //Normalde parola ve username için ayrı ayrı hata mesajı dönülmemesi gerekemektedir
@@ -40,12 +40,12 @@ userRouter.post("/login", async (req, res) => {
     if (!userFromDb) {
       return res
         .status(404)
-        .send({ status: false, message: "Username is not found" });
+        .send({ success: false, message: "Username is not found" });
     }
     if (password !== userFromDb.password) {
       return res
         .status(404)
-        .send({ status: false, message: "Inccorect Passoword" });
+        .send({ success: false, message: "Inccorect Passoword" });
     }
     //Token İşlemi
     let access_token = jwt.sign(
@@ -54,31 +54,31 @@ userRouter.post("/login", async (req, res) => {
       { expiresIn: "2h" }
     );
     res.status(200).send({
-      status: true,
+      success: true,
       message: `Welcome ${userFromDb.username}`,
       user: userFromDb,
       access_token: access_token,
     });
   } catch (error) {
-    res.status(400).send({ status: false, message: error.message });
+    res.status(400).send({ success: false, message: error.message });
   }
 });
 
 userRouter.get("/get-all", async (req, res) => {
   try {
     const users = await User.find({});
-    res.status(200).send({ status: true, message: "All Users", users: users });
+    res.status(200).send({ success: true, message: "All Users", users: users });
   } catch (error) {
-    res.status(400).send({ status: false, message: error.message });
+    res.status(400).send({ success: false, message: error.message });
   }
 });
 
 // userRouter.get("/get-all", tokenControl, async (req, res) => {
 //   try {
 //     const users = await User.find({});
-//     res.status(200).send({ status: true, message: "All Users", users: users });
+//     res.status(200).send({ success: true, message: "All Users", users: users });
 //   } catch (error) {
-//     res.status(400).send({ status: false, message: error.message });
+//     res.status(400).send({ success: false, message: error.message });
 //   }
 // });
 
@@ -90,7 +90,7 @@ userRouter.post("/reset-password-request", async (req, res) => {
     if (!user) {
       return res
         .status(404)
-        .send({ status: false, message: "User Not Found!" });
+        .send({ success: false, message: "User Not Found!" });
     }
 
     const token = CryptoJS.lib.WordArray.random(32).toString(CryptoJS.enc.Hex);
@@ -125,12 +125,12 @@ userRouter.post("/reset-password-request", async (req, res) => {
     transport.sendMail(mailOptions, (error) => {
       if (error) {
         console.log(error);
-        return res.status(400).send({ status: false, message: error.message });
+        return res.status(400).send({ success: false, message: error.message });
       }
-      res.status(200).send({ status: true, message: "Email Sent!" });
+      res.status(200).send({ success: true, message: "Email Sent!" });
     });
   } catch (error) {
-    res.status(400).send({ status: false, message: error.message });
+    res.status(400).send({ success: false, message: error.message });
   }
 });
 
@@ -139,12 +139,12 @@ userRouter.post("/reset-password/:token", async (req, res) => {
     const { password, passwordConfirmation } = req.body;
     const { token } = req.params;
     if (!token) {
-      return res.status(404).send({ status: false, message: "Invalid Token" });
+      return res.status(404).send({ success: false, message: "Invalid Token" });
     }
     if (password !== passwordConfirmation) {
       return res
         .status(404)
-        .send({ status: false, message: "Password not match" });
+        .send({ success: false, message: "Password not match" });
     }
     let user = await User.findOne({
       userPasswordToken: token,
@@ -153,16 +153,16 @@ userRouter.post("/reset-password/:token", async (req, res) => {
     if (!user) {
       return res
         .status(404)
-        .send({ status: false, message: "Invalid Token or Expired Token" });
+        .send({ success: false, message: "Invalid Token or Expired Token" });
     }
     user.password = password;
     user.userPasswordToken = null;
     user.userPasswordTokenTime = null;
     await user.save();
 
-    res.status(200).send({ status: true, message: "Password Changed!" });
+    res.status(200).send({ success: true, message: "Password Changed!" });
   } catch (error) {
-    res.status(400).send({ status: false, message: error.message });
+    res.status(400).send({ success: false, message: error.message });
   }
 });
 
