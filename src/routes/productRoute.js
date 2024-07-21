537db8025be0e23395e9bd8d100b6142e20d8cb8
Hasan Yurdakul <hhasanyurdakul@gmail.com>
2024-07-22 01:58:22 +0300

    add product router
    added endpoints and product router

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,1 +1,87 @@
-    
+const express = require("express");
+const Product = require("../models/productModel");
+
+const productRouter = express.Router();
+
+productRouter.post("/add", async (req, res) => {
+  try {
+    const { name, price, description, image, quantity } = req.body;
+    if (!name || !price || !description || !image || !quantity) {
+      return res
+        .status(404)
+        .send({ success: false, message: "Cannot leave empty fields!" });
+    }
+    let product = await Product.create(req.body);
+    res.status(200).send({
+      success: true,
+      message: "Product successfully created.",
+      product: product,
+    });
+  } catch (error) {
+    res.status(400).send({ success: false, message: error.message });
+  }
+});
+
+productRouter.get("/get-all", async (req, res) => {
+  try {
+    let products = await Product.find({});
+    res.status(200).send({
+      success: true,
+      message: "All products successfully fetched",
+      products: products,
+    });
+  } catch (error) {
+    res.status(400).send({ success: false, message: error.message });
+  }
+});
+
+productRouter.get("/get-by-id/:id", async (req, res) => {
+  console.log(req.params);
+  try {
+    let id = req.params.id;
+    let product = await Product.findById(id);
+    console.log(product);
+    res.status(200).send({
+      success: true,
+      message: "Product successfully fetched",
+      product: product,
+    });
+  } catch (error) {
+    res.status(400).send({ success: false, message: error.message });
+  }
+});
+
+productRouter.put("/update", async (req, res) => {
+  try {
+    const { id } = req.body;
+    // if (!name || !price || !description || !image || !quantity) {
+    //   return res
+    //     .status(404)
+    //     .send({ success: false, message: "Cannot leave empty fields!" });
+    // }
+    let product = await Product.findByIdAndUpdate(id, req.body);
+    res.status(200).send({
+      success: true,
+      message: "Product successfully updated.",
+      product: product,
+    });
+  } catch (error) {
+    res.status(400).send({ success: false, message: error.message });
+  }
+});
+
+productRouter.delete("/delete", async (req, res) => {
+  try {
+    const { id } = req.body;
+    let product = await Product.findByIdAndDelete(id);
+    res.status(200).send({
+      success: true,
+      message: "Product successfully deleted.",
+      product: product,
+    });
+  } catch (error) {
+    res.status(400).send({ success: false, message: error.message });
+  }
+});
+
+module.exports = productRouter;
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const userRouter = require("./routes/userRoute");
+const productRouter = require("./routes/productRoute");
 require("dotenv").config();
 const { MONGO_URL, PORT } = process.env;
 
@@ -10,6 +11,7 @@ app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
